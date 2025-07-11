df9da48bc37de53728ce53e17adb13eb862bdf86
kshubham <test.kshubham@gmail>
2025-07-11 23:00:09 +0530

    second commit

--- src/main/java/com/example/E_Note/Service/CategoryService.java
+++ src/main/java/com/example/E_Note/Service/CategoryService.java
@@ -5,6 +5,7 @@ import com.example.E_Note.Entity.Category;
 import java.util.List;
 
 public interface CategoryService {
+
    public Boolean saveCategory(Category category);
    public List<Category> getAllCategories();
 }
