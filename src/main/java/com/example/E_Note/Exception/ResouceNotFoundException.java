1c8c9184af04e92e67aad6fed991b93d1f58b019
kshubham <test.kshubham@gmail>
2025-07-31 13:30:38 +0530

    fourth commit

---
+++ src/main/java/com/example/E_Note/Exception/ResouceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.E_Note.Exception;
+
+public class ResouceNotFoundException extends RuntimeException{
+    public ResouceNotFoundException(String mssg){
+        super(mssg);
+    }
+}
